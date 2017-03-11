7b85519a2ded0f9e9c3847e0e806a5cf282293dd
Mathew Peterson <mpeterson@dealerinspire.com>
2017-03-11 13:11:21 -0600

    Adds default vagrant provider

--- .custom-env.dist.sh
+++ .custom-env.dist.sh
@@ -1,4 +1,7 @@
 export DOCKER_DNS_DOMAIN="docker.local"
 
 export ATLAS_TOKEN=""
+
 export HOMEBREW_GITHUB_API_TOKEN=""
+
+export VAGRANT_DEFAULT_PROVIDER="vmware_fusion" # vmware_fusion, vmware_workstation, virtualbox
